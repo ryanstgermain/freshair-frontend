255db25c67a84ce97fa965d35e6a17001984101e
= <treyyoung8@gmail.com>
2018-12-19 10:12:12 -0700

    added nonfunctioning basic html layout for trip page

--- FRESHAIRide-app/src/app/app.module.ts
+++ FRESHAIRide-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TripPageComponent } from './trip-page/trip-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripPageComponent
   ],
   imports: [
     BrowserModule,

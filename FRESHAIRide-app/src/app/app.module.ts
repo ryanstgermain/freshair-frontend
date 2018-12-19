743daf1c4480d3c7a93eaa892378739b96e1bffb
ryanstgermain <ryanjoel26@gmail.com>
2018-12-19 10:23:54 -0700

    got basic template for user homepage

--- FRESHAIRide-app/src/app/app.module.ts
+++ FRESHAIRide-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHomepageComponent
   ],
   imports: [
     BrowserModule,

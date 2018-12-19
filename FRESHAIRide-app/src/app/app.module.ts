26e34aae63378a3a1e9a0c27e0a18fca82553c83
xwrexx <wrex1285@gmail.com>
2018-12-19 13:45:13 -0700

    have log in card, working on styling

--- FRESHAIRide-app/src/app/app.module.ts
+++ FRESHAIRide-app/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { TripCreationComponent } from './trip-creation/trip-creation.component';
 import { TripPageComponent } from './trip-page/trip-page.component';
 import { UserHomepageComponent } from './user-homepage/user-homepage.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TripCreationComponent
+    TripCreationComponent,
     TripPageComponent,
     UserHomepageComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,

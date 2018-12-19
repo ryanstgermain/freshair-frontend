23f35bf7c9f0c9930aeee77465c49545d7b0a575
xwrexx <wrex1285@gmail.com>
2018-12-19 11:02:27 -0700

    boot strap working acros all components

--- FRESHAIRide-app/src/app/app-routing.module.ts
+++ FRESHAIRide-app/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  
+  imports: [
+    RouterModule.forRoot(routes),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

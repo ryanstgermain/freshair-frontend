964260dd829ea5629f68ddb3b65adf532ff6ffd2
thatnewcarsmell <scalajr@gmail.com>
2018-12-19 11:59:58 -0700

    should be all good now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { TripPageComponent } from './trip-page/trip-page.component';
 import { UserHomepageComponent } from './user-homepage/user-homepage.component';
 import { HomeComponent } from './home/home.component';

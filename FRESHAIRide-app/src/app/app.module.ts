3abafadfe7db550c622073870d99c056aa5069db
thatnewcarsmell <scalajr@gmail.com>
2018-12-19 11:25:29 -0700

    created a component module for the trip creation/joining page

--- FRESHAIRide-app/src/app/app.module.ts
+++ FRESHAIRide-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TripCreationComponent } from './trip-creation/trip-creation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripCreationComponent
   ],
   imports: [
     BrowserModule,
---
+++ FRESHAIRide-app/src/app/trip-creation/trip-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-creation',
+  templateUrl: './trip-creation.component.html',
+  styleUrls: ['./trip-creation.component.scss']
+})
+export class TripCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
